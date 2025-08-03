0c5c3aeb5cdcd36f0e5c038346035261829cc1c0
Anton Isakov <tonias@yandex.ru>
2025-08-03 14:45:56 +0300

    [FIX] Simplification of the code and correction of warnings for nullable variables.

--- ConsoleFramework/Binding/Converters/ConversionResult.cs
+++ ConsoleFramework/Binding/Converters/ConversionResult.cs
@@ -7,31 +7,21 @@ namespace ConsoleFramework.Binding.Converters;
 /// </summary>
 public class ConversionResult
 {
-    private readonly Object value;
-    private readonly bool success;
-    private readonly String failReason;
+    public object? Value { get; }
 
-    public object Value {
-        get { return value; }
-    }
+    public bool Success { get; }
 
-    public bool Success {
-        get { return success; }
-    }
-
-    public string FailReason {
-        get { return failReason; }
-    }
+    public string? FailReason { get; }
 
     public ConversionResult(Object value)
     {
-        this.value = value;
-        this.success = true;
+        Value = value;
+        Success = true;
     }
 
     public ConversionResult(bool success, String failReason)
     {
-        this.success = success;
-        this.failReason = failReason;
+        Success = success;
+        FailReason = failReason;
     }
 }
