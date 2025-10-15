ce27db37a7d31071914294ea3d880edddbaf394a
Anton Isakov <tonias@yandex.ru>
2025-10-15 22:44:00 +0300

    Simplify Cancel property in CancelEventArgs class
    Refactored the `Cancel` property in the `CancelEventArgs` class by removing the private field `_cancel` and its getter/setter methods. The property is now an auto-implemented property, resulting in cleaner and more concise code.

--- ConsoleFramework/Events/CancelEventArgs.cs
+++ ConsoleFramework/Events/CancelEventArgs.cs
@@ -4,15 +4,9 @@ public delegate void CancelEventHandler(object sender, CancelEventArgs e);
 
 public class CancelEventArgs : RoutedEventArgs
 {
-    private bool _cancel;
-
     public CancelEventArgs(object source, RoutedEvent routedEvent) : base(source, routedEvent)
     {
     }
 
-    public bool Cancel
-    {
-        get { return _cancel; }
-        set { _cancel = value; }
-    }
+    public bool Cancel { get; set; }
 }
